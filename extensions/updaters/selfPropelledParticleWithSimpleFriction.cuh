d3bb62f7ce22e6caf2c99b9ada606e589a9bf038
Haicen Yue <haicen.yue@uvm.edu>
2025-06-18 20:07:56 -0400

    debug the gpu code, but still not efficient enough.

--- extensions/updaters/selfPropelledParticleWithSimpleFriction.cuh
+++ extensions/updaters/selfPropelledParticleWithSimpleFriction.cuh
@@ -1,26 +1,34 @@
 #ifndef SELF_PROPELLED_PARTICLE_WITH_SIMPLE_FRICTION_CUH
 #define SELF_PROPELLED_PARTICLE_WITH_SIMPLE_FRICTION_CUH
 
-#include "std_include.h"
-#include <cuda_runtime.h>
-#include <cusolverSp.h>
-#include <cusparse.h>
+#include "selfPropelledParticleWithSimpleFriction.h"
+
+
 int gpu_computeRowPtr(
                     const int* d_nn,
                     int N,
                     int* d_row_ptr);
 bool gpu_spp_friction_eom_integration(
                     const int *neighborNum,
+                    const int *neighbors,
                     const double2 *forces,
                     double2 *velocities,
+                    double *velocity_flat,
+                    double *totalf_flat,
                     double2 *displacements,
                     const double2 *motility,
                     double *cellDirectors,
+                    int *d_row_ptr,
+                    int *d_col_ind,
+                    double *d_values,
+                    int *d_row_sizes,
                     curandState *RNGs,
                     int N,
                     Index2D n_idx,
                     double deltaT,
                     int Timestep,
                     double xi_sub,
-                    double xi_rel);
+                    double xi_rel,
+                    cusolverSpHandle_t handle,
+                    cusparseMatDescr_t descrA);
 #endif // SELF_PROPELLED_PARTICLE_WITH_SIMPLE_FRICTION_CUH
--- extensions/updaters/selfPropelledParticleWithSimpleFriction.h
+++ extensions/updaters/selfPropelledParticleWithSimpleFriction.h
@@ -2,18 +2,55 @@
 #define SELF_PROPELLED_PARTICLE_WITH_SIMPLE_FRICTION_H
 
 #include "selfPropelledParticleDynamics.h"
-
+#include "std_include.h"
+#include <cuda_runtime.h>
+#include <cusolverSp.h>
+#include <cusparse.h>
+#include <thrust/device_vector.h>
+#include <thrust/device_ptr.h>
+#include <thrust/scan.h>
+#include <thrust/reduce.h>
+#include <thrust/execution_policy.h>
+#include <math.h>
 #include <Eigen/SparseCore>
 
 class selfPropelledParticleWithSimpleFriction : public selfPropelledParticleDynamics
 {
 public:
     // Constructor
-    selfPropelledParticleWithSimpleFriction(int _N, double _xi_rel, bool _useGPU = true) 
+    selfPropelledParticleWithSimpleFriction(int _N, double _xi_rel = 1.0, bool _useGPU = true) 
         : selfPropelledParticleDynamics{_N, _useGPU}
         , xi_rel{_xi_rel}
     {
+        int maxRows = 2 * _N;
+        int maxNnz = 7 * maxRows; // Maximum non-zero entries in the friction matrix
+        // Allocate memory for the sparse matrix representation on the device
+        cudaMalloc(&d_row_ptr, (maxRows + 1) * sizeof(int));
+        cudaMemset(d_row_ptr, 0, sizeof(int));
+        cudaMalloc(&d_col_idx, maxNnz * sizeof(int));
+        cudaMalloc(&d_values, maxNnz * sizeof(double));
+        cudaMalloc(&velocity_flat, maxRows * sizeof(double));
+        cudaMalloc(&totalf_flat,  maxRows * sizeof(double));
+        cudaMalloc(&d_row_sizes,  maxRows * sizeof(int));
+        // create and initialize cuSolver/cuSparse handles
+        cusolverStatus_t cusolver_status = cusolverSpCreate(&handle);
+        cusparseCreateMatDescr(&descrA);
+        cusparseSetMatType(descrA, CUSPARSE_MATRIX_TYPE_GENERAL);
+        cusparseSetMatIndexBase(descrA, CUSPARSE_INDEX_BASE_ZERO);
+        // cusparseSetMatDiagType(descrA, CUSPARSE_DIAG_TYPE_NON_UNIT);
+        // cusparseSetMatFillMode(descrA, CUSPARSE_FILL_MODE_LOWER);
     } 
+    ~selfPropelledParticleWithSimpleFriction() 
+    {
+        if(d_row_ptr) cudaFree(d_row_ptr);
+        if(d_col_idx) cudaFree(d_col_idx);
+        if(d_values) cudaFree(d_values);
+        if(velocity_flat) cudaFree(velocity_flat);
+        if(totalf_flat) cudaFree(totalf_flat);
+        if(d_row_sizes) cudaFree(d_row_sizes);
+        cusparseDestroyMatDescr(descrA);
+        cusolverSpDestroy(handle);
+    }
     // Add public methods and members here
     virtual void integrateEquationsOfMotionCPU(); 
     virtual void integrateEquationsOfMotionGPU();
@@ -22,8 +59,16 @@ public:
 protected:
     // Add protected members here
     virtual void computeFrictionMatrix(Eigen::SparseMatrix<double> &mat);
+    int *d_row_sizes = nullptr; // Row sizes for sparse matrix
+    int *d_row_ptr = nullptr; // Row pointer for sparse matrix
+    int *d_col_idx = nullptr; // Column indices for sparse matrix
+    double *d_values = nullptr; // Values for sparse matrix
+    double *velocity_flat = nullptr; // Flattened velocity array for GPU
+    double *totalf_flat = nullptr; // Flattened total force array for GPU
     double xi_rel; // Relative friction coefficient
     double xi_sub = 1.; // Substrate Friction coefficient
+    cusolverSpHandle_t handle = nullptr; // cuSolver handle
+    cusparseMatDescr_t descrA = nullptr; // cuSparse matrix descriptor
 };
 
 #endif // SELF_PROPELLED_PARTICLE_WITH_SIMPLE_FRICTION_H
